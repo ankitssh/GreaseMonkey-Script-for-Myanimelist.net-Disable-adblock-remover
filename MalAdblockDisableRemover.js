4ae72ddecf42c533c9b9a1b8c56450c7a676d7b5
soulxhacker <godotcoffee231@gmail.com>
2017-02-18 18:35:06 +0530

    Initial Commit

---
+++ MalAdblockDisableRemover.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        MAL AD BLOCK
+// @namespace   Myanimelist
+// @version     1
+// @grant       none
+// ==/UserScript==
+for(i=0;i<9;i++)
+document.getElementsByClassName("_unit")[i].setAttribute("style","display:none");
